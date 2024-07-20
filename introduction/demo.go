f4665a150930364596eedf7db8dc69b1a68afd16
usman <usman2003.fb@gmail.com>
2024-07-20 00:31:43 -0700

    print function

---
+++ introduction/demo.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Call the printMessage function with a sample message
+	printMessage("Hello, World!")
+}
+
+// Function to print a message
+func printMessage(message string) {
+	fmt.Println(message)
+}
